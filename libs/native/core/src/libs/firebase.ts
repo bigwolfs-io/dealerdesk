955bb6fc5db2d1576437d4dd123fd94907a53c59
Hassan Azhar <hassanazharkhan@gmail.com>
2023-09-08 16:17:04 +0500

    feat(native/core): add firebase auth with hook

--- libs/native/core/src/libs/firebase.ts
+++ libs/native/core/src/libs/firebase.ts
@@ -31,3 +31,76 @@ export const auth = initializeAuth(app, {
 
 });
 
+interface User {
+  uid: string;
+  email: string | null;
+  displayName: string | null;
+}
+
+export const TOKEN_KEY = 'token'
+
+export const useFirebaseAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const signInWithEmailAndPassword = async (email: string, password: string) => {
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await _signInWithEmailAndPassword(auth, email, password);
+    } catch (error: any) {
+      console.log(error)
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUpWithEmailAndPassword = async (email: string, password: string) => {
+    try {
+      setLoading(true);
+      setError(null);
+      const res = await _createUserWithEmailAndPassword(auth, email, password);
+    } catch (error: any) {
+      console.log(error)
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      await _signOut(auth);
+    } catch (error: any) {
+      console.log(error)
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    user,
+    loading,
+    error,
+    signInWithEmailAndPassword,
+    signUpWithEmailAndPassword,
+    signOut,
+  };
+};
+
