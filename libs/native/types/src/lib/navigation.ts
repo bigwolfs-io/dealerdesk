9151f7ed9746ca7f749d27e64d804f426d14fcaf
Hassan Azhar <hassanazharkhan@gmail.com>
2023-09-08 17:32:39 +0500

    feat(ui): add swicth field component

--- libs/native/types/src/lib/navigation.ts
+++ libs/native/types/src/lib/navigation.ts
@@ -1,4 +1,9 @@
+import { ITask } from '@dealerdesk/shared/types'
 export type RootStackParamList = {
   SignUp: undefined,
   SignIn: undefined,
+  TaskCreate: undefined,
+  TaskUpdate: {
+    task: ITask
+  }
 }
--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,6 @@
 
+export * from './lib/switch-field/switch-field';
+
 export * from './lib/text-field/text-field';
 
 export * from './lib/button/button';
